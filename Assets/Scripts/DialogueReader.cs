2527f2f44e0891d11bc207d8c9677b312f2ab991
soulraiden <alexio.schneeberger@gmail.com>
2020-02-01 15:37:04 +0100

    FileReader

---
+++ Assets/Scripts/DialogueReader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class DialogueReader : MonoBehaviour
+{
+    public List<string> ReadDialog(string room, string location, string name)
+    {
+        List<string> text = new List<string>();
+        string line = null;
+        using (StreamReader file = new StreamReader("Assets/Rooms/" + room + "/" + location + "/" + name + ".txt"))
+        {
+            while ((line = file.ReadLine()) != null)
+                text.Add(line);
+        }
+        return text;
+    }
+}
