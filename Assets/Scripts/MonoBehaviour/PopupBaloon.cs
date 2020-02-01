c085ceb695d1cc0451c736e119c7f94966f0b59c
Marco Coccato <coccato.m@gmail.com>
2020-02-01 12:59:03 +0100

    Added Text mesh Pro. Created baloon script and prefab.

---
+++ Assets/Scripts/MonoBehaviour/PopupBaloon.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using TMPro;
+
+public class PopupBaloon : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI textArea;  
+    
+    public void SetText(string text)
+    {
+        textArea.text = text;
+    }
+}
