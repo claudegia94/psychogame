69ca81e4615b1288bd98c602bfcdea240a3b85cf
Marco Coccato <coccato.m@gmail.com>
2020-02-02 14:07:23 +0100

    Added end game GUI

--- Assets/Scripts/MonoBehaviour/EndGameGUIController.cs
+++ Assets/Scripts/MonoBehaviour/EndGameGUIController.cs
@@ -1,8 +1,63 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class EndGameGUIController : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject gui;
+
+    [Header("Fade Parameters")]
+    [SerializeField]
+    private float fadeIncrement = 0.2f; 
+    [SerializeField]
+    private float fadeTick = 0.2f;
+
+    private Image panel;
+
+    private void Awake() 
+    {
+        panel = GetComponent<Image>();    
+    }
+
+    public void FadeIn()
+    {
+        StopAllCoroutines();
+        StartCoroutine(FadeCorutine(fadeIncrement, fadeTick));
+    }
+
+    public void FadeOut()
+    {
+        SetGuiActive(false);
+        StopAllCoroutines();
+        StartCoroutine(FadeCorutine(-fadeIncrement,fadeTick));
+    }
+
+    public IEnumerator FadeCorutine(float increment, float tick)
+    {
+        Color c = panel.color;
+
+        do
+        {
+            Debug.Log("Fade alpha " + c.a);
+            c.a += increment;
+            panel.color = c;
+        
+            yield return new WaitForSeconds(tick);
+        } while(c.a > 0f && c.a < 1f);
+
+        if(increment > 0)
+        {
+            SetGuiActive(true);
+        }
+    }
+
+    public void SetGuiActive(bool value)
+    {
+        gui.SetActive(value);
+    }
+
    public void PlayAgain()
    {
        int sceneIndex = SceneManager.GetActiveScene().buildIndex;
@@ -12,5 +67,18 @@ public class EndGameGUIController : MonoBehaviour
    public void QuitGame()
    {
        Application.Quit();
+   }
+
+   private void OnValidate() 
+   {
+       if(fadeTick < 0)
+       {
+           fadeTick = 0f;
+       }
+
+       if(fadeIncrement < 0)
+       {
+           fadeIncrement = 0.05f;
+       }
    }
 }
---
+++ Assets/Scripts/MonoBehaviour/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private EndGameGUIController endGameGUI;
+
+    private ObjectManager objectManager;
+
+    private bool isGameEnded = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectManager = GetComponent<ObjectManager>();
+        endGameGUI.FadeOut();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(objectManager.CheckVictoryCondition() && !isGameEnded)
+        {
+            endGameGUI.FadeIn();
+            isGameEnded = true;
+        }
+    }
+}
